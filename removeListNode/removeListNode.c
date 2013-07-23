c3f87caa95d9dd746980a75cac5d82948059d09f
codeprepper <codeprepper@gmail.com>
2013-07-23 15:09:25 -0700

    added unit test cases

--- removeListNode/removeListNode.c
+++ removeListNode/removeListNode.c
@@ -10,22 +10,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Node {
     int val;
     struct Node *next;
 } Node;
 
-void removeListNode(Node *head, int rmVal) {
+void removeListNode(Node **head, int rval) {
     /* 1. remove leading nodes containing search value */
-    while (head != NULL && head->val == rmVal) {
-        Node *tmp = head;
-        head = head->next;
+    while (*head != NULL && (*head)->val == rval) {
+        Node *tmp = *head;
+        *head = (*head)->next;
         free(tmp);
     }
 
     /* 2. if the list is empty after (1) above, we're done */
-    if (head == NULL) return;
+    if (*head == NULL) return;
 
     /**
      * 3. walk through the list and delete any remaining nodes containing the
@@ -33,9 +34,9 @@ void removeListNode(Node *head, int rmVal) {
      *    does NOT contain the search value. We have to only check if 
      *    head->next (if it exists) contains the search value.
     **/
-    Node *current = head;
+    Node *current = *head;
     while (current->next != NULL) {
-        if (current->next->val == rmVal) {
+        if (current->next->val == rval) {
             /* next node contains search value; delete it */
             Node *tmp = current->next;
             current->next = tmp->next;
@@ -48,7 +49,7 @@ void removeListNode(Node *head, int rmVal) {
 
 void printList(Node *head) {
     if (head == NULL) {
-        printf("list is empty\n");
+        printf("list is empty");
         return;
     }
 
@@ -59,8 +60,66 @@ void printList(Node *head) {
     } while (current != NULL);
 }
 
+void appendToList(Node **head, Node *node) {
+    if (node == NULL) { return; }
+    node->next = NULL;
+
+    /* if the list is empty; this is the first node */
+    if (*head == NULL) {
+        *head = node;
+        return;
+    }
+
+    /* walk to the end of the list */
+    Node *tmp = *head;
+    while (tmp->next != NULL) { tmp = tmp->next; }
+        
+    /* tmp is pointing to the last node in the list */
+    tmp->next = node;
+}
+
+void destroyList(Node **head) {
+    while (*head != NULL) {
+        Node *tmp = *head;
+        *head = (*head)->next;
+        free(tmp);
+    }
+}
+
 int main(void) {
-    /* read test cases and call solution */
-    removeListNode(NULL, 5);
-    printList(NULL);
+    char str[100], *token;
+    Node *head;
+    int  rval, start;
+
+    /* line format: <delete-val> val1 val2 val3 .. */
+    while (fgets(str, 100, stdin) != NULL) {
+        head = NULL;
+        start = 1;
+        for (token = strtok(str, " ");
+             token != NULL;
+             token = strtok(NULL, " "))
+        {
+            /* read delete-val first */
+            if (start) {
+                rval = atoi(token);
+                start = 0;
+                continue;
+            }
+
+            /* create list node from value */
+            //printf("%d ", atoi(token));
+            Node *node;
+            node = (Node *)malloc(sizeof(Node));
+            node->val = atoi(token);
+            node->next = NULL;
+
+            /* append list node to existing list pointed to by 'head' */
+            appendToList(&head, node);
+        }
+
+        /* remove nodes containing rval, print resulting list and cleanup */
+        removeListNode(&head, rval);
+        printList(head); printf("\n");
+        destroyList(head);
+    }
 }
