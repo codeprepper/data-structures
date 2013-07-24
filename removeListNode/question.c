05c13897ffbda5f635821222d2c2cdd58372fed2
codeprepper <codeprepper@gmail.com>
2013-07-24 11:24:08 -0700

    cleaning up removeListNode

---
+++ removeListNode/question.c
@@ -0,0 +1,25 @@
+/**
+ * problem:
+ *      Remove all nodes from a linked-list containing a given
+ *      integer value.
+ * examples:
+ *      remove 3 from 12345 => 1245
+ *      remove 5 from 35456755 => 3467 
+ * input:
+ *      head: pointer to the head of a linked list
+ *      rval: the value that should be removed
+**/
+
+#include "test.h"
+
+/*
+typedef struct Node {
+    int         val;
+    struct Node *next;
+} Node;
+*/
+
+void removeListNode(Node **head, int rval) {
+    /* your code here */
+}
+
